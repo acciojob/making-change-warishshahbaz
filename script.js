cc384c6f143403851c41ba93a2edcbd7b9fbfc47
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-01-31 19:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const makeChange = (c) => {
-  // your name here
+  let = {
+	  quater:25,
+	  dime:10,
+	  nickel:5,
+	  penny:1,
+  }
+	let q = Math.floor(c/quater);
+	let remainingQuater = quaterAmount % quater;
+	let d = Math.floor(remainingQuater/dime);
+	let remainingDime = remainingQuater % dime;
+	let n = Math.floor(remainingDime /nickel);
+	let remainingNickel = remainingDime % nickel;
+	let p = Math.floor(remainingNickel/penny);
+
+	 const result = {
+        q: q,
+        d: d,
+        n: n,
+        p: p
+    };
+
+    return result;
 };
 
 // Do not the change the code below
